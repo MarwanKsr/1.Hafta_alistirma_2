8a019d43526a05f313f2b96f5b6fd24fa182aff2
Marwan <Marwan.kaseer1997@gmail.com>
2022-02-21 00:33:22 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace alistirme_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1.sayi: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("2.sayi: ");
+            int b = int.Parse(Console.ReadLine());
+            int c = a+b;
+            Console.WriteLine("{0} ve {1} sayisinin toplami {2}",a,b,c);
+        }
+    }
+}
